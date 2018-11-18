ae7742324e1d38376ffd2ecb18877e6b457f0d85
Javier Santos <javiermestre96@gmail.com>
2018-11-18 17:36:31 +0100

    @migueldavidorrico mirame estos ejercicios, son los dos primeros.

--- src/Espejo.java
+++ src/Espejo.java
@@ -1,5 +1,25 @@
+/*
+Cadena espejo. Crea un programa Espejo.java que reciba una cadena de caracteres y la devuelva invertida con efecto espejo,
+esto es, se concatena a la palabra original su inversa, compartiendo la última letra, que hará de espejo, por lo que
+la palabra obtenida se lee igual hacia adelante que hacia atrás.
+Por ejemplo, al introducir “teclado” devolverá “tecladodalcet” y al introducir “goma” devolverá “gomamog”
+Usará un método espejoCadena
+ */
+
+
 public class Espejo {
     public static String espejoCadena(String cadena) {
-        return null;
+        int longitudCadena=cadena.length();
+        String salida=cadena;
+        for(int i=longitudCadena-2;i>=0;i--) {
+            salida += cadena.charAt(i);
+        }
+        return salida;
+    }
+
+    public static void main(String[] args){
+
+        System.out.println(espejoCadena("teclado"));
+
     }
 }
--- src/Inversa.java
+++ src/Inversa.java
@@ -1,5 +1,20 @@
+/*
+Crear un programa llamado Inversa.java que pida una cadena al usuario y la devuelva invertida.
+Usará un método inversaCadena static que tiene como parámetro la cadena a invertir y devuelve la cadena invertida
+ */
+
+
 public class Inversa {
     public static String inversaCadena(String cadena) {
-        return null;
+        int longitudCadena=cadena.length();
+        String salida="";
+        for(int i=longitudCadena-1;i>=0;i--){
+            salida+=cadena.charAt(i);
+        }
+        return salida;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(inversaCadena("periquito"));
     }
 }
